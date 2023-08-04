f105da133d96be04619d69f7473998cb2281c175
HabeebaNazrin <habeebanazrin1406@gmail.com>
2023-08-04 23:58:50 +0530

    second commit

--- lib/assignment/splash.dart
+++ lib/assignment/splash.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
 
-class splash2 extends StatefulWidget {
-  @override
-  State<splash2> createState() => splash2state();
+void main() {
+  runApp(MaterialApp(
+    home: splash2(),
+  ));
 }
-class splash2state extends State<splash2>{
-  @override
-  
 
+class splash2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/icons/images/background image.png"))),
+        child: const Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.food_bank_sharp,
+              size: 50,
+              color: Colors.white,
+            ),
+            Text(
+              " fresh foods...!",
+              style: TextStyle(
+                  fontSize: 60,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.white),
+            ),
+            Text(
+              "Tasty and Healthy",
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  fontStyle: FontStyle.italic,
+                  color: Colors.white),
+            )
+          ],
+        )),
+      ),
+    );
+  }
 }
